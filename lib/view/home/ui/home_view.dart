a5b5d6ffc9d0d42c580686b476284dbfad135900
isafty <ealsafty@gmail.com>
2025-10-08 15:29:01 +0300

    search bar

--- lib/view/home/ui/home_view.dart
+++ lib/view/home/ui/home_view.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:new_app_1/core/widget/textformfield1.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Home View'));
+    return ListView(
+      children: const [
+        SizedBox(height: 20),
+        TFF1(
+          ltext: 'Search',
+          prefixIcon: Icon(Icons.search),
+          textInputType: TextInputType.text,
+          suffixIcon: IconButton(onPressed: null, icon: Icon(Icons.send)),
+        ),
+        SizedBox(height: 20),
+        Center(child: Icon(Icons.storefront, size: 200, color: Colors.grey)),
+      ],
+    );
   }
 }
